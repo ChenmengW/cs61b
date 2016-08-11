fe25ad1fac926ce6d162934f87cb1b80b3269e1c
varad <varad@aquarius.com>
2016-08-11 21:48:56 +0530

    hw2 completed

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -4,4 +4,123 @@ import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
 
+    private int N;
+    private int numOpenSites;
+    private boolean[][] openSites;
+
+
+    private WeightedQuickUnionUF disjointSets;
+    private WeightedQuickUnionUF connectedToTop;
+
+    private int virtualTop;
+    private int virtualBottom;
+
+    private int indexXYTo1D(int x, int y) {
+        return (x * N) + y;
+    }
+
+    public Percolation(int N) {                 // create N-by-N grid, with all sites initially blocked
+        if (N <= 0) {
+            throw new IllegalArgumentException("N must be positive");
+        }
+
+        this.N = N;
+        this.numOpenSites = 0;
+        this.openSites = new boolean[N][N];     // all are initialized to false by default
+
+        int numSets = (N * N) + 2;              // + 2 for virtual top & bottom
+        this.disjointSets = new WeightedQuickUnionUF(numSets);   // each element in its own set
+        this.connectedToTop = new WeightedQuickUnionUF(numSets - 1);    // only virtual top, no bottom
+
+        // Make the last 2 elements of the WQUUF the virtual
+        // top and bottom
+        this.virtualTop = numSets - 2;
+        this.virtualBottom = numSets - 1;
+
+    }
+
+    public void open(int row, int col) {       // open the site (row, col) if it is not open already
+        if (row < 0 || row >= this.N ||
+                col < 0 || col >= this.N) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+
+        openSites[row][col] = true;
+        int index1D = this.indexXYTo1D(row, col);
+
+        // Connect top row to virtual top and bottom row to
+        // virtual bottom
+
+        if (row == 0) {
+            this.disjointSets.union(index1D, this.virtualTop);
+            this.connectedToTop.union(index1D, this.virtualTop);
+        }
+
+        if (row == this.N - 1) {
+            this.disjointSets.union(index1D, this.virtualBottom);
+        }
+
+
+        // Check if neighbors are open. If yes, connect them.
+        // Omit the checks for boundary cells obviously.
+
+        if (row > 0 && isOpen(row - 1, col)) {                     // up
+            int upIndex1D = this.indexXYTo1D(row - 1, col);
+            this.disjointSets.union(index1D, upIndex1D);
+            this.connectedToTop.union(index1D, upIndex1D);
+        }
+
+        if (row < this.N - 1 && isOpen(row + 1, col)) {            // down
+            int downIndex1D = this.indexXYTo1D(row + 1, col);
+            this.disjointSets.union(index1D, downIndex1D);
+            this.connectedToTop.union(index1D, downIndex1D);
+        }
+
+        if (col > 0 && isOpen(row, col - 1)) {                     // left
+            int leftIndex1D = this.indexXYTo1D(row, col - 1);
+            this.disjointSets.union(index1D, leftIndex1D);
+            this.connectedToTop.union(index1D, leftIndex1D);
+        }
+
+        if (col < this.N - 1 && isOpen(row, col + 1)) {            // right
+            int rightIndex1D = this.indexXYTo1D(row, col + 1);
+            this.disjointSets.union(index1D, rightIndex1D);
+            this.connectedToTop.union(index1D, rightIndex1D);
+        }
+
+        this.numOpenSites += 1;
+    }
+
+    public boolean isOpen(int row, int col) {  // is the site (row, col) open?
+        if (row < 0 || row >= this.N ||
+                col < 0 || col >= this.N) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+
+        return openSites[row][col];
+    }
+
+    public boolean isFull(int row, int col) {  // is the site (row, col) full?
+        if (row < 0 || row >= this.N ||
+                col < 0 || col >= this.N) {
+            throw new IllegalArgumentException("Index out of bounds");
+        }
+
+        int index1D = this.indexXYTo1D(row, col);
+        return this.disjointSets.connected(index1D, this.virtualTop)
+                && this.connectedToTop.connected(index1D, this.virtualTop);
+    }
+
+    public int numberOfOpenSites() {           // number of open sites
+        return this.numOpenSites;
+    }
+
+    public boolean percolates() {              // does the system percolate?
+        return this.disjointSets.connected(this.virtualBottom, this.virtualTop);
+    }
+    public static void main(String[] args){    // unit testing (not required)
+//        Percolation p = new Percolation(5);
+//        p.open(0, 3);
+//        p.open(1, 3);
+    }
 }                       
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,96 @@
-package hw2;                       
+package hw2;
+
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+import java.util.ArrayList;
 
 public class PercolationStats {
+
+    private int N;
+    private int T;
+    private ArrayList<Double> threshold;
+
+    public PercolationStats(int N, int T) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException("arguments cannot be zero");
+        }
+
+        this.N = N;
+        this.T = T;
+        this.threshold = this.runSimulation();
+    }
+
+    private double runSinglePass() {
+        Percolation p = new Percolation(this.N);
+        int row = StdRandom.uniform(0, this.N);
+        int col = StdRandom.uniform(0, this.N);
+
+        while (!p.percolates()) {
+
+            // get a blocked site
+            while (!p.isOpen(row, col)) {
+                row = StdRandom.uniform(0, this.N);
+                col = StdRandom.uniform(0, this.N);
+            }
+
+            p.open(row, col);
+        }
+
+        double thresh = p.numberOfOpenSites() / this.N * this.N;
+        return thresh;
+    }
+
+    private ArrayList<Double> runSimulation() {
+        ArrayList<Double> thresh = new ArrayList<Double>();
+
+        for (int i = 0; i < this.T; i++) {
+            double n = runSinglePass();
+            thresh.add(n);
+        }
+
+        return thresh;
+    }
+
+    public double mean() {
+        double mu = 0;
+
+        for (int i = 0; i < this.T; i++) {
+            mu += this.threshold.get(i);
+        }
+
+        mu /= this.T;
+        return mu;
+    }
+
+    public double stddev() {
+        double mu = this.mean();
+        double sigmasq = 0;
+
+        for (int i = 0; i < this.T; i++) {
+            double term =  this.threshold.get(i) - mu;
+            term = Math.pow(term, 2);
+            sigmasq += term;
+        }
+
+        sigmasq /= this.T - 1;
+        return sigmasq;
+    }
+
+    public double confidenceLow() {
+        double mu = this.mean();
+        double sigma = Math.sqrt(this.stddev());
+
+        double lowEndPoint = mu - ((1.96 * sigma) / Math.sqrt(this.T));
+        return lowEndPoint;
+    }
+
+    public double confidenceHigh() {
+        double mu = this.mean();
+        double sigma = Math.sqrt(this.stddev());
+
+        double highEndPoint = mu + ((1.96 * sigma) / Math.sqrt(this.T));
+        return highEndPoint;
+    }
 
 }                       
